712ae54a661a1d5886fc1e3e2a25a70b146e9784
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-09-23 11:12:59 -0400

    Version brute

---
+++ README.md
@@ -0,0 +1,13 @@
+# des
+Exercice préparatoire à la première épreuve
+
+## Description
+Description à venir
+
+## Particularités
+
+- Compléter
+
+## Variantes et ajouts possibles
+
+- Compléter
---
+++ src/App.js
@@ -0,0 +1,45 @@
+/**
+ * @module App
+ */
+export default class App {
+	/**
+	 * Méthode principale. Sera appelée après le chargement de la page.
+	 */
+	static main() {
+		document.getElementById("btnLancer").addEventListener("click", e => {
+			this.lancer();
+		});
+	}
+	static lancer() {
+		// Récupération des données du formulaire
+		var joueur = document.getElementById("joueur");
+		var nbDes = joueur.nbDes.valueAsNumber;
+		var couleur = joueur.couleur.value;
+		// Affichage
+		var table = document.getElementById("table");
+		while (table.firstChild) {
+			table.removeChild(table.firstChild);
+		}
+		for (let i = 0; i < nbDes; i += 1) {
+			let n = Math.floor(Math.random() * 6) + 1;
+			let c = couleur;
+			if (c === "a") {
+				let lesCouleurs = ["r", "v", "b"];
+				let posCouleur = Math.floor(Math.random() * lesCouleurs.length);
+				c = lesCouleurs[posCouleur];
+			}
+			let img = table.appendChild(document.createElement("img"));
+			img.src = "images/d"+c+""+n+".svg";
+		}
+	}
+	/**
+	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
+	 * @returns undefined Ne retourne rien
+	 */
+	static init() {
+		window.addEventListener("load", () => {
+			this.main();
+		});
+	}
+}
+App.init();
